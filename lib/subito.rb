e79bfeb71332284e6519697eb6034d5a7684611c
Michael Kessler <michi@netzpiraten.ch>
2009-01-23 23:55:23 +0100

    Keep it simple stupid and start slowly

--- lib/subito.rb
+++ lib/subito.rb
@@ -1,23 +1,17 @@
-$:.unshift File.dirname(__FILE__)
-
 require "rubygems"
 require "simpleconsole"
 require 'pathname'
 require "subito/project"
 require "subito/submodule"
 
-Dir.glob(File.join(File.dirname(__FILE__), 'subito/*.rb')).each {|f| require f }
-
 class Controller < SimpleConsole::Controller
 
   before_filter :locate_project
 
   def default
-    @message = "Hello World!"
   end
 
   def status
-
   end
 
   private
@@ -35,12 +29,9 @@ class View < SimpleConsole::View
   end
 
   def status
-
     @project.submodules.each do |s|
       puts s
     end
   end
 
 end
-
-SimpleConsole::Application.run(ARGV, Controller, View)
