ff69ffdf1cefa53e6a880230c78c5e36cae18037
Michael Kessler <michi@netzpiraten.ch>
2009-05-21 00:05:12 +0200

    Writing now correct bundles. First parsing for the installation process (not yet done).

--- lib/helper/bundle.rb
+++ lib/helper/bundle.rb
@@ -12,16 +12,16 @@ class Bundle
       file.write("---\n")
       project.extensions.each do |extension|
         
-        puts "***".blue + "Process extension ".dark_green + "'#{extension.to_s}'".yellow if options.verbose?
-        
         file.write("- name: #{extension.to_s}\n")
-        file.write("  hub: #{hub(extension.remotes['origin'])}\n")
+        file.write("  hub: #{hub(extension.to_s, extension.remotes['origin'])}\n")
+
+        extension.remotes.delete('origin')
         
         if options.remotes?
+          file.write("  remotes:\n")
           extension.remotes.each do |name, url|
-            remote = hub(url, true)
-            file.write("  - remote: #{remote}\n") if name != 'origin'
-            puts "  - Remote".white + "'#{remote}'".cyan + "registered".white if options.verbose?
+            remote = hub(extension.to_s, url, true)
+            file.write("    - #{remote}\n")
           end
         end
       
@@ -29,7 +29,8 @@ class Bundle
       
       puts "Bundle written to " + BUNDLE
     end
-    
+
+    File.open(project.root + BUNDLE).each { |line| puts line } if options.verbose?
   end
   
   #
@@ -38,19 +39,33 @@ class Bundle
   #   +options+ the command line options
   #
   def self.install(project, options)
-    puts "Not yet implemented"
-    exit 1
+    bundle = YAML::load_file(project.root + BUNDLE)
+    bundle.each do |extension|
+      name = extension['name']
+      if not project.extensions.map{|e| e.to_s }.include?(name)
+        puts "TODO: Installing" + name
+      end
+      if extension['remotes']
+        extension['remotes'].each do |remote|
+          Dir.chdir(project.root + "vendor/extensions" + name) do
+            puts "TODO: Register " + remote
+          end
+        end
+      end
+    end
   end
   
   private
   
     #
     # Gets the hub from the repositories url
+    #   +name+ name of the extension
     #   +url+ the url to the extensions repository
+    #   +remote+ true if remote
     #
-    def self.hub(url, remote = false)
+    def self.hub(name, url, remote = false)
       if url =~ /git(@|:\/\/)github.com(:|\/)([a-zA-z0-9_\-]+)\/([a-zA-z0-9_\-]+).git/
-        if remote
+        if remote and not known_format?(Regexp.last_match(4), name)
           return Regexp.last_match(3) + '/' + Regexp.last_match(4)
         else
           return Regexp.last_match(3)
@@ -59,5 +74,32 @@ class Bundle
         return url
       end
     end
-  
+
+    #
+    # Is the format of the radiant extension naming known?
+    #   +full_name+ full name of the extension, eg. radiant-comments-extension
+    #   +name+ name of the extension, e.g. comments
+    #
+    def self.known_format?(full_name, name)
+      decline_radiant_extension(name).include?(full_name)
+    end
+
+    #
+    # Returns an array of known radiant extension naming variants
+    #   +name+ name of the extension
+    #
+    def self.decline_radiant_extension(name)
+       [
+         "radiant-#{name}-extension",
+         "radiant-#{name.to_s.gsub('_','-')}-extension",
+         "radinat-#{name}",
+         "radiant-#{name.to_s.gsub('_','-')}",
+         "#{name}",
+         "#{name}.gsub('_','-')",
+         "radiant_#{name}_extension",
+         "radiant_#{name.to_s.gsub('_','-')}_extension",
+         "radinat_#{name}",
+         "radiant_#{name.to_s.gsub('_','-')}",
+       ]
+    end
 end
